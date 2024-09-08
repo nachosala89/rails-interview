a5df32e6f5253daa580ee88dff7442cae500fb21
Juan Ignacio Sala <ignacio.sala@lisainsurtech.com>
2024-09-08 15:47:19 -0300

    Add basic CRUD for web

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -12,4 +12,11 @@ class TodoListsController < ApplicationController
 
     respond_to :html
   end
+
+  # GET /todolists/1 or /todolists/1.json
+  def show
+    @todo_list = TodoList.find(params[:id])
+
+    respond_to :html
+  end
 end
